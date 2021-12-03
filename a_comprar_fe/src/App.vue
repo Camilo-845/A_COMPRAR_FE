6af36c641e4adf74742457a5d90ad7f0ab32b9ab
Camilo S <camilo.sarmiento.amado@gmail.com>
2021-12-03 09:51:57 -0500

    configuracion proyecto V.1

--- a_comprar_fe/src/App.vue
+++ a_comprar_fe/src/App.vue
@@ -1,30 +1,55 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app" class="app">
+    <div class="header">
+      <h1>Banco UN</h1>
+      <nav>
+        <button v-if="is_auth">Inicio</button>
+        <button v-if="is_auth">Cuenta</button>
+        <button v-if="is_auth">Cerrar Sesión</button>
+        <button v-if="!is_auth" v-on:click="loadLogIn">Iniciar Sesión</button>
+        <button v-if="!is_auth" v-on:click="loadSignUp">Registrarse</button>
+      </nav>
+    </div>
+    <div class="main-component">
+      <router-view
+        v-on:completedLogIn="completedLogIn"
+        v-on:completedSignUp="completedSignUp"
+      >
+      </router-view>
+    </div>
+    <div class="footer">
+      <h2>Misión TIC 2022</h2>
+    </div>
   </div>
-  <router-view/>
 </template>
 
+<script>
+export default {
+  name: "App",
+  data: function () {
+    return {
+      is_auth: false,
+    };
+  },
+  components: {},
+  methods: {
+    verifyAuth: function () {
+      if (this.is_auth == false) this.$router.push({ name: "logIn" });
+    },
+    loadLogIn: function () {
+      this.$router.push({ name: "logIn" });
+    },
+    loadSignUp: function () {
+      this.$router.push({ name: "signUp" });
+    },
+    completedLogIn: function (data) {},
+    completedSignUp: function (data) {},
+  },
+  created: function () {
+    this.verifyAuth();
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
